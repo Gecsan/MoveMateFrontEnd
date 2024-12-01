35c65003965743827ca114b2e4bd3affdfd7d1b4
Gecsan <greyes2054@sdsu.edu>
2024-11-30 18:46:18 -0800

    adding saved files

---
+++ movemate-frontend-main/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className="flex h-screen w-full items-center justify-center gap-2">
+        <span>
+          <i className="fa-solid fa-people-carry-box !text-3xl"></i>
+          <p className="  font-bold text-2xl">MOVEMATE FRONTEND SETUP</p>
+        </span>
+      </div>
+    </>
+  );
+}
+
+export default App;
